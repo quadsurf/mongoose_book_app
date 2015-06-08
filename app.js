500d4b3f913fd215b824c2cba237f1a51293afb3
Elie Schoppik <eschoppik@gmail.com>
2015-06-07 22:36:32 -0700

    refactoring

--- app.js
+++ app.js
@@ -68,7 +68,6 @@ app.put('/authors/:id', function(req,res){
  db.Author.findByIdAndUpdate(req.params.id, {firstName: req.body.firstName,lastName: req.body.lastName},
      function (err, author) {
        if(err) {
-         console.log(err);
          res.render("authors/edit");
        }
        else {
@@ -96,16 +95,21 @@ app.delete('/authors/:id', function(req,res){
 
 // INDEX
 app.get('/authors/:author_id/books', function(req,res){
-  db.Author.findById(req.params.author_id,
-    function (err, author) {
-      db.Book.find(
-      {
-        _id: {$in: author.books}
-      },
-      function(err, books){
-        res.render("books/index", {author:author, books:books});
-      });
-    });
+  // db.Author.findById(req.params.author_id,
+  //   function (err, author) {
+  //     db.Book.find(
+  //     {
+  //       _id: {$in: author.books}
+  //     },
+  //     function(err, books){
+  //       res.render("books/index", {author:author, books:books});
+  //     });
+  //   });
+
+  // REFACTOR USING POPULATE
+  db.Author.findById(req.params.author_id).populate('books').exec(function(err,author){
+    res.render("books/index", {author:author});
+  });
 });
 
 // NEW
@@ -126,7 +130,7 @@ app.post('/authors/:author_id/books', function(req,res){
     else {
       db.Author.findById(req.params.author_id,function(err,author){
         author.books.push(book);
-        book.author_id = author._id;
+        book.author = author._id;
         book.save();
         author.save();
         res.redirect("/authors/"+ req.params.author_id +"/books");
@@ -137,24 +141,39 @@ app.post('/authors/:author_id/books', function(req,res){
 
 // SHOW
 app.get('/authors/:author_id/books/:id', function(req,res){
-  db.Author.findById(req.params.author_id,
-      function (err, author) {
-        db.Book.findById(req.params.id,
-        function(err, book){
-          res.render("books/show", {author:author,book:book});
-        });
-      });
+  // db.Author.findById(req.params.author_id,
+  //     function (err, author) {
+  //       db.Book.findById(req.params.id,
+  //       function(err, book){
+  //         res.render("books/show", {author:author,book:book});
+  //       });
+  //     });
+
+  // REFACTOR USING POPULATE
+  db.Book.findById(req.params.id)
+    .populate('author')
+    .exec(function(err,book){
+      console.log(book.author)
+      res.render("books/show", {book:book});
+    });
 });
 
 // EDIT
 app.get('/authors/:author_id/books/:id/edit', function(req,res){
-  db.Author.findById(req.params.author_id,
-      function (err, author) {
-        db.Book.findById(req.params.id,
-        function(err, book){
-          res.render("books/edit", {author:author,book:book});
-        });
-      });
+  // db.Author.findById(req.params.author_id,
+  //     function (err, author) {
+  //       db.Book.findById(req.params.id,
+  //       function(err, book){
+  //         res.render("books/edit", {author:author,book:book});
+  //       });
+  //     });
+
+  // REFACTOR USING POPULATE
+  db.Book.findById(req.params.id)
+    .populate('author')
+    .exec(function(err,book){
+      res.render("books/edit", {book:book});
+    });
 });
 
 // UPDATE
@@ -162,7 +181,6 @@ app.put('/authors/:author_id/books/:id', function(req,res){
  db.Book.findByIdAndUpdate(req.params.id, {title:req.body.title},
      function (err, book) {
        if(err) {
-         console.log(err);
          res.render("books/edit");
        }
        else {
--- models/book.js
+++ models/book.js
@@ -2,9 +2,8 @@ var mongoose = require("mongoose");
 
 var bookSchema = new mongoose.Schema({
                     title: String,
-                    author: String,
                     description: String,
-                    author_id: {
+                    author: {
                       type: mongoose.Schema.Types.ObjectId,
                       ref: "Author"
                     }
